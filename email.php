72161d8267c84d0504693ce10a7054c14fb16779
Trey Cluff <treycluff@gmail.com>
2020-02-11 16:21:51 -0500

    saving temp

--- email.php
+++ email.php
@@ -1,11 +1,8 @@
 <?php
 print "<h2>PHP is Fun!</h2>";
-$sendee = $_POST["email"];
-$subject = $_POST["subject"];
-$body = $_POST["body"];
-$txt = $sendee + " " + $subject + " " + $body;
-$testfile = fopen("testfile.txt", "w") or die("Unable to open file!");
-fwrite($testfile, $txt);
-fclose($testfile);
+$sendee = htmlspecialchars($_POST["email"]);
+$subject = htmlspecialchars($_POST["subject"]);
+$body = htmlspecialchars($_POST["body"]);
+echo "<h2>"+$sendee+"</h2>";
 
 ?>
