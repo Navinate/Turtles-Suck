ce8ab1b02aac7339b5d2c761b98c7264edeec88c
Trey Cluff <treycluff@gmail.com>
2020-02-12 15:58:52 -0500

    finally got php to work, wrong cancate for strings

--- email.php
+++ email.php
@@ -3,6 +3,5 @@ print "<h2>PHP is Fun!</h2>";
 $sendee = htmlspecialchars($_POST["email"]);
 $subject = htmlspecialchars($_POST["subject"]);
 $body = htmlspecialchars($_POST["body"]);
-echo "<h2>"+$sendee+"</h2>";
-
+echo "<h2>" . $sendee . "</h2>";
 ?>
