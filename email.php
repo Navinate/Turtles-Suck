bcde26780a83dd18074a158b12e5aaa5c27f3e86
Navinate <treycluff@gmail.com>
2020-05-20 04:02:31 -0400

    Delete email.php

--- email.php
+++
@@ -1,8 +0,0 @@
-<?php
-print "<h2>PHP is Fun!</h2>";
-$email = htmlspecialchars($_POST["email"]);
-$subject = htmlspecialchars($_POST["subject"]);
-$body = htmlspecialchars($_POST["body"]);
-$password = htmlspecialchars($_POST["password"]);
-echo "<h2>Sending to " . $email . "</h2>";
-?>
